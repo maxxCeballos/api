20dce8d5d39a77cbc8ebf83d84f487db6ca49290
Maxi Ceballos <maxi@MBP-de-Maxi.fibertel.com.ar>
2021-12-12 23:04:17 -0300

    add case test

--- lib-status-code/index.js
+++ lib-status-code/index.js
@@ -4,38 +4,34 @@
  * Status Responses Centralized
  */
 
-
 const statusCodes = {
-    OK: 200,
-    CREATED: 201,
-    BAD_REQUEST: 400,
-    UNAUTHORIZED: 401,
-    NOT_FOUND: 404,
-    CONFLICT: 409,
-    BLOCKED_INVOICE: 3000,
-    DISPATCHED_INVOICE: 3001,
+  OK: 200,
+  CREATED: 201,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  NOT_FOUND: 404,
+  CONFLICT: 409,
+  BLOCKED_INVOICE: 3000,
+  DISPATCHED_INVOICE: 3001,
 };
 
 const statusReason = {
-    OK: 'Ok',
-    CREATED: 'Created',
-    BAD_REQUEST: 'Bad Request',
-    UNAUTHORIZED: 'Unauthorized',
-    NOT_FOUND: 'Not Found',
-    CONFLICT: 'Conflict',
-    BLOCKED_INVOICE: 'The invoice could not be generated',
-    DISPATCHED_INVOICE: 'Invoice dispatched to customer',
+  OK: 'Ok',
+  CREATED: 'Created',
+  BAD_REQUEST: 'Bad Request',
+  UNAUTHORIZED: 'Unauthorized',
+  NOT_FOUND: 'Not Found',
+  CONFLICT: 'Conflict',
+  BLOCKED_INVOICE: 'The invoice could not be generated',
+  DISPATCHED_INVOICE: 'Invoice dispatched to customer',
 };
-  
+
 Object.freeze(statusCodes);
 Object.freeze(statusReason);
 
 module.exports = {
-    statusCodes,
-    statusReason,
+  statusCodes,
+  statusReason,
 };
 
 
-
-
-  
--- lib-status-code/index.test.js
+++ lib-status-code/index.test.js
@@ -7,114 +7,117 @@ const {statusCodes, statusReason} = require('./index');
  * TEST CODES
  */
 describe('Code by Name', () => {
+  const testsCodes = [
+    {
+      name: 'should return true',
+      input: Object.isFrozen(statusCodes),
+      expected: true,
+    },
+    {
+      name: 'should return 200',
+      input: statusCodes.OK,
+      expected: 200,
+    },
+    {
+      name: 'should return 201',
+      input: statusCodes.CREATED,
+      expected: 201,
+    },
+    {
+      name: 'should return 400',
+      input: statusCodes.BAD_REQUEST,
+      expected: 400,
+    },
+    {
+      name: 'should return 401',
+      input: statusCodes.UNAUTHORIZED,
+      expected: 401,
+    },
+    {
+      name: 'should return 404',
+      input: statusCodes.NOT_FOUND,
+      expected: 404,
+    },
+    {
+      name: 'should return 409',
+      input: statusCodes.CONFLICT,
+      expected: 409,
+    },
+    {
+      name: 'should return 3000',
+      input: statusCodes.BLOCKED_INVOICE,
+      expected: 3000,
+    },
+    {
+      name: 'should return 3001',
+      input: statusCodes.DISPATCHED_INVOICE,
+      expected: 3001,
+    },
+  ];
 
-    const testsCodes = [
-      {
-        name: 'should return 200',
-        input: statusCodes.OK,
-        expected: 200,
-      },
-      {
-        name: 'should return 201',
-        input: statusCodes.CREATED,
-        expected: 201,
-      },
-      {
-        name: 'should return 400',
-        input: statusCodes.BAD_REQUEST,
-        expected: 400,
-      },
-      {
-        name: 'should return 401',
-        input: statusCodes.UNAUTHORIZED,
-        expected: 401,
-      },
-      {
-        name: 'should return 404',
-        input: statusCodes.NOT_FOUND,
-        expected: 404,
-      },
-      {
-        name: 'should return 409',
-        input: statusCodes.CONFLICT,
-        expected: 409,
-      },
-      {
-        name: 'should return 3000',
-        input: statusCodes.BLOCKED_INVOICE,
-        expected: 3000,
-      },
-      {
-        name: 'should return 3001',
-        input: statusCodes.DISPATCHED_INVOICE,
-        expected: 3001,
-      },
-    ];
-
-    testsCodes.forEach(test => {
-      it(test.name, () => {
-        
-        expect(test.input).toBe(test.expected);
-
-      });
+  testsCodes.forEach((test) => {
+    it(test.name, () => {
+      expect(test.input).toBe(test.expected);
     });
+  });
 });
-
 
 
 /**
  * TEST REASON PHRASES
  */
 describe('Reason Phrase by Name', () => {
-  
-    const testsStatusReason = [
-      {
-        name: 'should return Ok',
-        input: statusReason.OK,
-        expected: 'Ok',
-      },
-      {
-        name: 'should return Created',
-        input: statusReason.CREATED,
-        expected: 'Created',
-      },
-      {
-        name: 'should return Bad Request',
-        input: statusReason.BAD_REQUEST,
-        expected: 'Bad Request',
-      },
-      {
-        name: 'should return Unauthorized',
-        input: statusReason.UNAUTHORIZED,
-        expected: 'Unauthorized',
-      },
-      {
-        name: 'should return Not Found',
-        input: statusReason.NOT_FOUND,
-        expected: 'Not Found',
-      },
-      {
-        name: 'should return Conflict',
-        input: statusReason.CONFLICT,
-        expected: 'Conflict',
-      },
-      {
-        name: 'should return The invoice could not be generated',
-        input: statusReason.BLOCKED_INVOICE,
-        expected: 'The invoice could not be generated',
-      },
-      {
-        name: 'should return Invoice dispatched to customer',
-        input: statusReason.DISPATCHED_INVOICE,
-        expected: 'Invoice dispatched to customer',
-      },
-    ];
+  const testsStatusReason = [
+    {
+      name: 'should return true',
+      input: Object.isFrozen(statusReason),
+      expected: true,
+    },
+    {
+      name: 'should return Ok',
+      input: statusReason.OK,
+      expected: 'Ok',
+    },
+    {
+      name: 'should return Created',
+      input: statusReason.CREATED,
+      expected: 'Created',
+    },
+    {
+      name: 'should return Bad Request',
+      input: statusReason.BAD_REQUEST,
+      expected: 'Bad Request',
+    },
+    {
+      name: 'should return Unauthorized',
+      input: statusReason.UNAUTHORIZED,
+      expected: 'Unauthorized',
+    },
+    {
+      name: 'should return Not Found',
+      input: statusReason.NOT_FOUND,
+      expected: 'Not Found',
+    },
+    {
+      name: 'should return Conflict',
+      input: statusReason.CONFLICT,
+      expected: 'Conflict',
+    },
+    {
+      name: 'should return The invoice could not be generated',
+      input: statusReason.BLOCKED_INVOICE,
+      expected: 'The invoice could not be generated',
+    },
+    {
+      name: 'should return Invoice dispatched to customer',
+      input: statusReason.DISPATCHED_INVOICE,
+      expected: 'Invoice dispatched to customer',
+    },
+  ];
 
-    testsStatusReason.forEach(test => {
-      it(test.name, () => {
-        
-        expect(test.input).toBe(test.expected);
-
-      });
+  testsStatusReason.forEach((test) => {
+    it(test.name, () => {
+      expect(test.input).toBe(test.expected);
     });
+  });
 });
